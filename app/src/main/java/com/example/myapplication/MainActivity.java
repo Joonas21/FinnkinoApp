b98e382dc94a25639bc1e60278a3e918ba920ea3
Joonas21 <joonas.kukonlehto@gmail.com>
2022-03-30 14:54:58 +0300

    Declared UI objects

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText startTimeInput;
+    EditText endTimeInput;
+    EditText dateInput;
+    EditText nameInput;
+    Spinner theaterSpinner;
+    ListView movies;
+    Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startTimeInput = (EditText) findViewById(R.id.startTime);
+        endTimeInput = (EditText) findViewById(R.id.endTime);
+        dateInput = (EditText) findViewById(R.id.date);
+        nameInput = (EditText) findViewById(R.id.name);
+        theaterSpinner = (Spinner) findViewById(R.id.theater);
+        movies = (ListView) findViewById(R.id.movies);
+        search = (Button) findViewById(R.id.search);
+
     }
 }
