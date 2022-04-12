dac9bb123ceb38d0fda54e6ff54f3296b8e28eaa
sandelsson <81624691+sandelsson@users.noreply.github.com>
2022-04-12 14:19:53 +0300

    review view fixed

--- app/src/main/java/com/example/myapplication/Review.java
+++ app/src/main/java/com/example/myapplication/Review.java
@@ -1,13 +1,22 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Review extends AppCompatActivity
-{
-    // constructor
-    public Review() {}
+public class Review extends AppCompatActivity {
 
-    // code here
+    public Review() {
+    }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.rating_layout);
+
+        // code here
+
+    }
 }
 
