8c8fb3223f17af1f577d937303e35a825bed77cf
Joonas21 <joonas.kukonlehto@gmail.com>
2022-04-12 14:32:21 +0300

    Declared Ui elements

--- app/src/main/java/com/example/myapplication/Review.java
+++ app/src/main/java/com/example/myapplication/Review.java
@@ -1,10 +1,24 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Review extends AppCompatActivity {
+
+    Button rate;
+    RatingBar ratingBar;
+    EditText nickname;
+    EditText dateOfRating;
+    EditText review;
+    ListView reviewsList;
+    TextView movie;
+
 
     public Review() {
     }
@@ -14,6 +28,14 @@ public class Review extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.rating_layout);
+
+        rate = (Button) findViewById(R.id.rate);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        nickname = (EditText) findViewById(R.id.nickname);
+        dateOfRating = (EditText) findViewById(R.id.dateOfRating);
+        review = (EditText) findViewById(R.id.review);
+        reviewsList = (ListView) findViewById(R.id.reviewsList);
+        movie = (TextView) findViewById(R.id.movie);
 
         // code here
 
